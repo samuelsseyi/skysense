46ee88444f39d7801ec414b1b5a87dc7e7e27728
SamuelSeyi <steadywords@gmail.com>
2024-04-08 16:59:07 -0700

    My First Commit For Version Weather App V2

---
+++ script.js
@@ -0,0 +1,55 @@
+const apiKey = "308b780e1a7653426dc43fe146ea7ebd";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.getElementById("searchBox") 
+const searchButton = document.getElementById("searchButton")
+
+let time = new Date().toLocaleTimeString()
+currentTime.innerHTML = time
+
+
+
+searchButton.addEventListener("click", (e)=>{
+  if (searchBox.value === "") {
+    document.getElementById("errorDiv").style.display = "block"
+    errorDiv.innerHTML = "Please Input city or Country Name"
+    setTimeout(()=>{
+      document.getElementById("errorDiv").style.display = "none"
+    },3000)
+
+  }
+  checkWeather(searchBox.value);
+
+  async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+  
+  
+    console.log(data);
+    document.getElementById("country").innerHTML = `${data.name}`
+    document.getElementById("temp").innerHTML = `${data.main.temp}⁰c`
+    document.getElementById("weatherCondition").innerHTML = data.weather[0].description
+    document.getElementById("icon").innerHTML = `<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png' alt='weather img' width="80px"/>`
+    document.getElementById("moreWeatherInfo").innerHTML = `
+    <p class="fs-4 border-bottom text-dark p-3">Country Code: ${data.sys.country}</p>
+    <p class="fs-4 border-bottom text-dark p-3">Pressure: ${data.main.pressure}hPa</p>
+    <p class="fs-4 border-bottom text-dark p-3">Humidity: ${data.main.humidity}% </p>
+    <p class="fs-4 border-bottom text-dark p-3">Wind Speed: ${data.wind.speed}m/s</p>
+    <p class="fs-4 border-bottom text-dark p-3">Latitude: ${data.coord.lat} </p>
+    <p class="fs-4 border-bottom text-dark p-3">Longitude: ${data.coord.lon} </p>
+    <p class="fs-4 border-bottom text-dark p-3">Min Temperature: ${data.main.temp_min}⁰c </p>
+    <p class="fs-4 border-bottom text-dark p-3">Max Temperature: ${data.main.temp_max}⁰c </p>
+    `
+  
+  }
+  
+  checkWeather()
+})
+
+
+
+
+// Current Time Code
+
+
+
